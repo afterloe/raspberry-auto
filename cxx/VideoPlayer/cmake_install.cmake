4aafb1fb8f6dbfc1bbacfb643aacea246c7c0b80
afterloe <lm6289511@gmail.com>
2019-10-29 09:08:10 +0800

    feat: Linux Ubuntu x86_x64

--- cxx/VideoPlayer/cmake_install.cmake
+++ cxx/VideoPlayer/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/pi/Project/LearnOpencv/cxx/VideoPlayer
+# Install script for directory: /home/afterloe/Project/LearnOpencv/cxx/VideoPlayer
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/pi/Project/LearnOpencv/cxx/VideoPlayer/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/afterloe/Project/LearnOpencv/cxx/VideoPlayer/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
