34138aeee045376bf7098849bd761b3e6ff0c463
afterloe <lm6289511@gmail.com>
2019-10-29 10:44:46 +0800

    feat: 绘制矩形的两种方法

--- cxx/DrawOperation/src/DrawOperation.cpp
+++ cxx/DrawOperation/src/DrawOperation.cpp
@@ -7,19 +7,21 @@ using namespace cv;
 #define WINDOWS_NAME "图片展示"
 
 namespace that {
-    int drawRect(const Mat&);
+    int drawRect(Mat&);
 }
 
 int main(int argc, char ** argv) {
     std::cout << "绘制矩形"
         << std::endl;
-    Mat image = Mat::zeros(800, 800, CV_32F);
+    Mat image = Mat::zeros(800, 800, CV_8UC3);
     return that::drawRect(image);
 }
 
-int that::drawRect(const Mat& image) {
+int that::drawRect(Mat& image) {
     Rect rec1 = Rect(100, 300, 600, 200);
-    rectangle(image, rec1, Scalar(0, 0, 255), -1, 8, 0);
+    // 虚线
+    rectangle(image, rec1, Scalar(0, 0, 255), 1, 1, 0);
+    rectangle(image, cv::Point(10, 200), cv::Point(50, 300), Scalar(255, 0, 0), -1, 8, 0);
     imshow(WINDOWS_NAME, image);
     waitKey();
     return 0;
