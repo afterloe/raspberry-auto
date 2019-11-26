5dfab74b96a3c5e369976f8c019940af05eb1321
afterloe <605728727@qq.com>
2019-11-26 15:57:19 +0800

    feat: 演示脚本编写

---
+++ Demonstration/clawControl.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from __future__ import division
+import time
+import Adafruit_PCA9685
+
+pwm = Adafruit_PCA9685.PCA9685()
+
+pwm.set_pwm(2, 0, 340)
+time.sleep(2)
+pwm.set_pwm(2, 0, 490)
+time.sleep(2)
+pwm.set_pwm(2, 0, 340)
---
+++ Demonstration/speech_tool/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+if "__mian__" == __name__:
+    print("主程序运行")
+else:
+    print("package init....")
